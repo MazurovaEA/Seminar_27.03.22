e7f9a8c06b2a86e8d0171088ab0554014b9d41eb
Katerina <ealmazurova@internet.ru>
2022-03-27 11:28:38 +0300

    Фибоначчи, Реверс, Копирование массива, перевод в двоичную систему

---
+++ Fibonacci/Program.cs
@@ -0,0 +1,34 @@
+﻿// Не используя рекурсию, выведите первые N чисел Фибоначчи. Первые два числа Фибоначчи: 0 и 1.
+
+int Promt(string message)
+{
+    Console.Write(message);
+    string strValue = Console.ReadLine();
+    int Value = int.Parse(strValue);
+    return Value;
+}
+
+void PrintArray(int[] collect)
+{
+    Console.Write("[");
+    for (int i = 0; i < collect.Length - 1; i++)
+    {
+        Console.Write(collect[i] + ",");
+
+    }
+    Console.Write(collect[collect.Length - 1] + "]");
+}
+int[] Fibo(int N)
+{
+    int[] K = new int[N];
+    K[0] = 0;
+    K[1] = 1;
+    for (int i = 2; i < N; i++)
+    {
+        K[i] = K[i - 1] + K[i - 2];
+    }
+    return K;
+}
+int N = Promt("Введите число => ");
+int[] K = Fibo(N);
+PrintArray(K);
